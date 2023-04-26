c71a9ec11eab30b883cf089814b36caf3ded0df1
eze stom <ezequielstom@gmail.com>
2023-04-26 19:08:48 -0300

    new config

---
+++ resume/js/script.js
@@ -0,0 +1,11 @@
+// Esperar a que se cargue la página completamente
+window.addEventListener('load', function() {
+   // Esperar 5 segundos y luego cambiar a la segunda vista
+
+   setTimeout(function() {
+     window.location.href = 'index2.html';
+   }, 7000); //8 segundos
+ });
+
+
+ 
