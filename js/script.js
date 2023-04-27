c42be5b0b6296b1fb3e6811b3c3724ffb6a88a0c
eze stom <ezequielstom@gmail.com>
2023-04-27 11:33:14 -0300

    100 vw

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Esperar a que se cargue la página completamente
 window.addEventListener('load', function() {
    // Esperar 5 segundos y luego cambiar a la segunda vista
+ 
 
    setTimeout(function() {
      window.location.href = 'index2.html';
