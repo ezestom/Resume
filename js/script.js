76321b0de91ee5d07473f0d18cb5e417d4671b29
eze stom <ezequielstom@gmail.com>
2023-04-27 19:46:01 -0300

    fix bg color

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // Esperar a que se cargue la página completamente
 window.addEventListener('load', function() {
-   // Esperar 5 segundos y luego cambiar a la segunda vista
+   // Esperar 7 segundos y luego cambiar a la segunda vista
  
 
    setTimeout(function() {
      window.location.href = 'index2.html';
-   }, 7000); //8 segundos
+   }, 7000); //7 segundos
  });
--- js/script2.js
+++ js/script2.js
@@ -1,15 +1,13 @@
- // activar background
+ // cambiar a modo oscuro
  window.addEventListener('load', function() {
 
-
-   
    function toggleClass() {
       const myBody = document.querySelector("body");
       myBody.classList.toggle("active");
       myBody.classList.toggle("desactive");
     }
     
-    var myButton = document.getElementById("checkbox");
+    const myButton = document.getElementById("checkbox");
     myButton.addEventListener("click", toggleClass);
     
 })
